a2f2056e8291971f44e7597e95c98b70beee497a
Pedro Liska <pedro.liska@extendhealth.com>
2013-09-06 09:01:40 -0600

    problem 10 complete

---
+++ euler/solutions/Problem10.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace euler.solutions
+{
+    public class Problem10
+    {
+        public static void Run()
+        {
+            int maxPrime = 2000000;
+
+            Stopwatch watch = Stopwatch.StartNew();
+
+            var sieve = new SieveOfPedro();
+            long sum = 0;
+            var nextPrime = 0;
+            while ((nextPrime = sieve.Next()) < maxPrime)
+            {
+                sum += nextPrime;
+            }
+
+            watch.Stop();
+            Console.WriteLine("{0} is the sum of all the primes below {1}", sum, maxPrime);
+            Console.WriteLine("it took {0} milliseconds to calcluate this", watch.ElapsedMilliseconds);
+        }
+    }
+}
