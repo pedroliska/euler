fc1f1146fa751e11a49f8e12181e5bd90f6823b8
Pedro Liska <pedro.liska@extendhealth.com>
2013-09-06 16:29:03 -0600

    began problem 12

--- euler/Program.cs
+++ euler/Program.cs
@@ -6,7 +6,7 @@ namespace euler
     {
         private static void Main(string[] args)
         {
-            Problem011.Run();
+            Problem012.Run();
         }
     }
 }
---
+++ euler/solutions/Problem012.cs
@@ -0,0 +1,25 @@
+﻿namespace euler.solutions
+{
+    public class Problem012
+    {
+        public static void Run()
+        {
+            var generator = new TriangularNumberGenerator();
+
+            while (true)
+            {
+                var number = generator.Next();
+            }
+        }
+
+        private class TriangularNumberGenerator
+        {
+            private int last = 0;
+
+            public int Next()
+            {
+                return last + ++last;
+            }
+        }
+    }
+}
