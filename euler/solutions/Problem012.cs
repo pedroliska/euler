39dfcbbefef0d67b227a838697b9b4ac9f22f805
Pedro Liska <pedro.liska@extendhealth.com>
2013-09-09 07:17:10 -0600

    think i finished but it takes forever to run

--- euler/solutions/Problem012.cs
+++ euler/solutions/Problem012.cs
@@ -1,24 +1,49 @@
-﻿namespace euler.solutions
+﻿using System;
+using System.Diagnostics;
+
+namespace euler.solutions
 {
     public class Problem012
     {
         public static void Run()
         {
+            var target = 500;
+
+            Stopwatch watch = Stopwatch.StartNew();
             var generator = new TriangularNumberGenerator();
 
-            while (true)
+            int number;
+            int divisorCount;
+            do
             {
-                var number = generator.Next();
+                number = generator.Next();
+                divisorCount = NumberOfDivisors(number);
+                Console.WriteLine("number: {0}  divisors {1}", number, divisorCount);
+            } while (divisorCount <= target);
+            
+            watch.Stop();
+            Console.WriteLine("{0} is the first triangle number to have over {1} divisors", number, target);
+            Console.WriteLine("it took {0} tics to calcluate this", watch.ElapsedTicks);
+        }
+
+        public static int NumberOfDivisors(int number)
+        {
+            int divisorCount = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                if (number%i == 0) divisorCount++;
             }
+            return divisorCount;
         }
 
         private class TriangularNumberGenerator
         {
-            private int last = 0;
+            private int _lastAddition;
+            private int _lastRetVal;
 
             public int Next()
             {
-                return last + ++last;
+                return _lastRetVal += ++_lastAddition;
             }
         }
     }
