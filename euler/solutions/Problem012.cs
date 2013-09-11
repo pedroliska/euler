a9557f43b62cd8fed28e79f2afe80247babe2119
Pedro Liska <pedro.liska@extendhealth.com>
2013-09-11 06:48:00 -0600

    optimized problem 12

--- euler/solutions/Problem012.cs
+++ euler/solutions/Problem012.cs
@@ -7,32 +7,31 @@ namespace euler.solutions
     {
         public static void Run()
         {
-            var target = 500;
+            int target = 500;
 
-            Stopwatch watch = Stopwatch.StartNew();
-            var generator = new TriangularNumberGenerator();
-
-            int number;
-            int divisorCount;
-            do
+            Timer.RecordMiliseconds(() =>
             {
-                number = generator.Next();
-                divisorCount = NumberOfDivisors(number);
-                Console.WriteLine("number: {0}  divisors {1}", number, divisorCount);
-            } while (divisorCount <= target);
-            
-            watch.Stop();
-            Console.WriteLine("{0} is the first triangle number to have over {1} divisors", number, target);
-            Console.WriteLine("it took {0} tics to calcluate this", watch.ElapsedTicks);
+                Stopwatch watch = Stopwatch.StartNew();
+                var generator = new TriangularNumberGenerator();
+
+                int number;
+                int divisorCount;
+                do
+                {
+                    number = generator.Next();
+                    divisorCount = NumberOfDivisors(number);
+                } while (divisorCount <= target);
+
+                Console.WriteLine("{0} is the first triangle number to have over {1} divisors", number, target);
+            });
         }
 
         public static int NumberOfDivisors(int number)
         {
             int divisorCount = 0;
-            for (int i = 1; i <= number; i++)
-            {
-                if (number%i == 0) divisorCount++;
-            }
+            for (int i = 1; i <= Math.Sqrt(number); i++)
+                if (number%i == 0) divisorCount+=2;
+
             return divisorCount;
         }
 
@@ -45,6 +44,22 @@ namespace euler.solutions
             {
                 return _lastRetVal += ++_lastAddition;
             }
+        }
+    }
+
+    public class TimerOld
+    {
+        private readonly Stopwatch _watch;
+
+        public TimerOld()
+        {
+            _watch = Stopwatch.StartNew();
+        }
+
+        public void ResultsInSeconds()
+        {
+            _watch.Stop();
+            Console.WriteLine("Completed in {0}ms", _watch.ElapsedMilliseconds);
         }
     }
 }
