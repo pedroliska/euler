490d273e919b27ada885403e3a88919d62d1d51a
Pedro Liska <pedro.liska@extendhealth.com>
2013-09-18 07:15:33 -0600

    finished problem 16

---
+++ euler/solutions/Problem016.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace euler.solutions
+{
+    public class Problem016
+    {
+        public static void Run()
+        {
+            Timer.RecordMiliseconds(() =>
+            {
+                BigInteger result = BigInteger.Pow(2, 1000);
+                int sum = 0;
+                foreach (char c in result.ToString())
+                    sum += (int)char.GetNumericValue(c);
+
+                Console.WriteLine(sum);
+            });
+        }
+    }
+}
