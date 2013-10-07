320fb55ab4e841d06d7fa7f08aa7545a3b152b23
Pedro Liska <pedro.liska@extendhealth.com>
2013-10-07 13:09:01 -0600

    finished problem 20

--- euler/Program.cs
+++ euler/Program.cs
@@ -6,7 +6,7 @@ namespace euler
     {
         private static void Main(string[] args)
         {
-            Problem019.Run();
+            Problem020.Run();
         }
     }
 }
---
+++ euler/solutions/Problem020.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace euler.solutions
+{
+    public class Problem020
+    {
+         public static void Run()
+         {
+             int number = 100;
+             var factorial = Factorial(number);
+             var sum = SumOfDigits(factorial);
+             Console.WriteLine("The factorial digit sum of {0}! is {1}", number, sum);
+         }
+        private static BigInteger Factorial(int number)
+        {
+            BigInteger start = 1;
+            return Enumerable.Range(1, number).Aggregate(start, (a,b) => a*b);
+        }
+        private static int SumOfDigits(BigInteger big)
+        {
+            return big.ToString().Select(c => int.Parse(c.ToString())).Aggregate(0, (a, b) => a + b);
+        }
+
+    }
+}
