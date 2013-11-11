a2cc2c1033e2d654a34e3d91b993c9d1bfddda3f
Pedro Liska <pedro.liska@extendhealth.com>
2013-11-11 09:55:04 -0700

    finished prob 24

--- euler/solutions/Problem024.cs
+++ euler/solutions/Problem024.cs
@@ -12,20 +12,24 @@ namespace euler.solutions
             //string digits = "123";
             //var items = new List<int> { 1, 2, 3 };
             var items = "0123456789".ToList();
+            var permutationPosition = 1000000;
 
-            var permutations = Permutate(items);
-            DisplayPermutations(permutations);
+            Timer.RecordMiliseconds(() =>
+            {
+                var permutation = Permutate(items).Skip(permutationPosition - 1).First();
+                Console.WriteLine("{0} is the {1}th permutation", PermutationToString(permutation), permutationPosition);
+            });
         }
 
-        private static List<List<T>> Permutate<T>(List<T> items)
+        private static IEnumerable<List<T>> Permutate<T>(List<T> items)
         {
             // this is when recursion ends
             if (items.Count <= 1)
             {
-                return new List<List<T>> { items };
+                yield return items;
+                yield break;
             }
 
-            var retVal = new List<List<T>>();
             for (int i = 0; i < items.Count; i++)
             {
                 var pivot = items[i];
@@ -38,23 +42,19 @@ namespace euler.solutions
                 {
                     var retItem = result.ToList();
                     retItem.Insert(0, pivot);
-                    retVal.Add(retItem);
+                    yield return retItem;
                 }
             }
-            return retVal;
         }
 
-        private static void DisplayPermutations<T>(List<List<T>> permutations)
+        private static void DisplayPermutations<T>(IEnumerable<List<T>> permutations)
         {
             foreach (var permutation in permutations)
-            {
-                //Console.WriteLine(
-                //    permutation.Aggregate((a, b) => a.ToString() + b.ToString())
-                //    );
-                foreach (var item in permutation)
-                    Console.Write(item);
-                Console.WriteLine();
-            }
+                Console.WriteLine(PermutationToString(permutation));
+        }
+        private static string PermutationToString<T>(IEnumerable<T> permutation)
+        {
+            return permutation.Aggregate("", (current, item) => current + item.ToString());
         }
     }
 }
